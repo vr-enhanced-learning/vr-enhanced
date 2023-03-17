8244b64f1cb4ad9ca72ee95438fde094b5d6fe0a
tharunoptimus <tharunoptimus@outlook.com>
2023-03-17 09:10:35 +0530

    requests mic permission onLoad

--- assets/js/app.js
+++ assets/js/app.js
@@ -188,3 +188,11 @@ setTimeout(() => {
 		if (i === questionWords.length) clearInterval(interval)
 	}, 150)
 }, 15000)
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(function(stream) {
+        console.log('You let me use your mic!')
+      })
+      .catch(function(err) {
+        console.log('No mic for you!')
+      });
