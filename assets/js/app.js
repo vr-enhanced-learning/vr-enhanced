90618c4e414bd32dbb2201c8eb406bb9d1c3c5e9
tharunoptimus <tharunoptimus@outlook.com>
2023-04-10 02:12:53 +0530

    Moved script to its own file re-writing app.js Did a prettier on the markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,144 +1,139 @@
-let actionButtons = document.querySelectorAll(".actionButtons")
-let statusField = document.querySelector("#statusField")
-let answerButton = document.querySelector("#answerButton")
-let stopListeningButton = document.querySelector("#stopListeningButton")
+const web2vr = new Web2VR("#html-container", {
+	position: {
+		x: 0,
+		y: 2.1,
+		z: -0.6,
+	},
+})
+web2vr.start()
 
-let QUESTION_ANSWER_SPACE_ENDPOINT =
-	"https://currentlyexhausted-mariorossi-t5-base-finetuned-que-ac173dd.hf.space/run/predict"
+let questionPanel = new Web2VR("#questionPanel", {
+	position: {
+		x: 0.05,
+		y: 2.1,
+		z: -1.2,
+	},
+	rotation: {
+		x: 0,
+		y: -45,
+		z: -2,
+	},
+})
+questionPanel.start()
 
-let WELCOME_STATUS_TEXT = "Tap on 'Create Context' or 'Create Question' to start..."
+let doubtPanel = new Web2VR("#doubtPanel", {
+	position: {
+		x: -1.3,
+		y: 1.8,
+		z: -0.5,
+	},
+	rotation: {
+		x: 0,
+		y: 45,
+		z: 2,
+	},
+})
+doubtPanel.start()
 
-function setStatus(text) {
-	statusField.setAttribute("text", "value: " + text)
-}
-
-function speechToText(button, inputField, statusText) {
-	let context = ""
-	let speechRecognition = new webkitSpeechRecognition()
-	speechRecognition.continuous = true
-	speechRecognition.interimResults = true
-	speechRecognition.lang = "en-US"
-	speechRecognition.onresult = function (event) {
-		let interimTranscript = ""
-		for (let i = event.resultIndex; i < event.results.length; i++) {
-			if (event.results[i].isFinal) {
-				context += event.results[i][0].transcript
-				inputField.setAttribute("text", "value: " + context)
-			} else {
-				interimTranscript += event.results[i][0].transcript
-				inputField.setAttribute(
-					"text",
-					"value: " + context + interimTranscript
-				)
-			}
-		}
-	}
-
-	button.addEventListener("click", function () {
-		context = ""
-		inputField.setAttribute("text", "value: ")
-		setStatus(statusText)
-		stopListeningButton.setAttribute("visible", "true")
-		speechRecognition.start()
-	})
-
-	return {speech: speechRecognition, inputField}
-}
-
-function stopListening(listenerObject, changeAttribute = false) {
-	listenerObject.speech.stop()
-	if(changeAttribute) listenerObject.inputField.setAttribute("text", "value: ")
-}
-
-function stopListentingUI(changeAttribute = false) {
-	stopListeningButton.setAttribute("visible", "false")
-	stopListening(contextListener, changeAttribute)
-	stopListening(questionListener, changeAttribute)
-	setStatus("Stopped listening")
-}
-
-let contextListener = speechToText(
-	document.querySelector("#contextButton"),
-	document.querySelector("#contextInputField"),
-	"Listening to context"
-)
-
-let questionListener = speechToText(
-	document.querySelector("#questionButton"),
-	document.querySelector("#questionInputField"),
-	"Listening to question"
-)
-
-function textToSpeech(text) {
-	let msg = new SpeechSynthesisUtterance()
-
-	let voices = window.speechSynthesis.getVoices()
-
-	voices.forEach((voice) => {
-		if (voice.name === "Google US English") {
-			msg.voice = voice
-		}
-	})
-
-	msg.text = text
-	msg.lang = "en-US"
-	window.speechSynthesis.speak(msg)
-}
-
-async function query(data) {
-	const response = await fetch(QUESTION_ANSWER_SPACE_ENDPOINT, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	})
-
-	const result = await response.json()
-	return result
-}
-
-function constructQuery(context, question) {
-	return {
-		data: [`question: ${question} context: ${context}`],
-	}
-}
-
-answerButton.addEventListener("click", async () => {
-	stopListentingUI(false)
-	setStatus("Parsing question and context...")
-
-	let context = document
-		.querySelector("#contextInputField")
-		.getAttribute("text").value
-	let question = document
-		.querySelector("#questionInputField")
-		.getAttribute("text").value
-
-	document
-		.querySelector("#answerInputField")
-		.setAttribute("text", "value: Generating.... Please wait")
-	textToSpeech("Generating.... Please wait")
-
-	let queryData = constructQuery(context, question)
-
-	setStatus("Generating answer...Please wait")
-	let result = await query(queryData)
-	console.log(result)
-
-	setStatus("Answer generated!")
-	let answer = result.data[0]
-	let predictionTime = result.duration.toFixed(2)
-
-	document
-		.querySelector("#answerInputField")
-		.setAttribute(
-			"text",
-			"value: " + answer + " (took " + predictionTime + " seconds)"
-		)
-	textToSpeech(answer)
+document.getElementById("enterVR").addEventListener("click", () => {
+	document.getElementById("a-scene").enterVR()
+	document.getElementById("enterVRButtonContainer").innerHTML = ""
 })
 
-stopListeningButton.addEventListener("click", () => {
-	stopListentingUI()
-})
+document
+	.getElementById("submitYoutubeId")
+	.addEventListener("click", async () => {
+		let videoId = document.getElementById("videoId").value
 
-setStatus(WELCOME_STATUS_TEXT)
+		if (videoId) {
+			document.getElementById("formContainer").innerHTML = ""
+
+			let videoTag = document.createElement("video")
+			videoTag.id = "video"
+			videoTag.src = `https://relay-youtube.adaptable.app/${videoId}`
+			videoTag.width = 640
+			videoTag.height = 360
+			videoTag.controls = true
+			videoTag.crossOrigin = "anonymous"
+
+			document.getElementById("content").appendChild(videoTag)
+
+			document.getElementById(
+				"liveStatus"
+			).innerHTML = `Status: Generating Questions...`
+
+			let questions = await fetch(
+				"https://youtube-questions.vercel.app",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						youtubeVideoId: videoId,
+					}),
+				}
+			)
+
+			questions = await questions.json()
+
+			document.getElementById(
+				"liveStatus"
+			).innerHTML = `Status: Questions Generated`
+
+			let questionsContainer = document.createElement("div")
+			questionsContainer.id = "questionsContainer"
+
+			let questionsDiv = document.createElement("div")
+			questionsDiv.id = "questions"
+
+			let imageTag = document.createElement("img")
+			imageTag.src = "./assets/images/question.png"
+			imageTag.alt = "question"
+			imageTag.height = "150"
+			imageTag.width = "300"
+
+			questionsContainer.appendChild(imageTag)
+
+			questions.forEach((question, index) => {
+				let questionSpan = document.createElement("span")
+				questionSpan.innerHTML = question
+
+				let answerInput = document.createElement("input")
+				answerInput.type = "text"
+				answerInput.placeholder = "Enter Answer"
+				answerInput.dataset.id = index + 1
+
+				questionsDiv.appendChild(questionSpan)
+				questionsDiv.appendChild(answerInput)
+			})
+
+			questionsContainer.appendChild(questionsDiv)
+
+			document.getElementById("questionPanel").prepend(questionsContainer)
+
+			document.getElementById(
+				"buttonsContainer"
+			).innerHTML = `<button id="submitAnswer" type="button">Submit Answers</button>`
+
+			setTimeout(() => {
+				document.getElementById("liveStatus").innerHTML = ""
+			}, 1000)
+
+			document
+				.getElementById("submitAnswer")
+				.addEventListener("click", async () => {
+					let answers = []
+					document
+						.querySelectorAll("#questions input")
+						.forEach((input) => {
+							answers.push({
+								questionId: input.dataset.id,
+								answer: input.value,
+							})
+						})
+
+					console.log(answers)
+				})
+		}
+	})
